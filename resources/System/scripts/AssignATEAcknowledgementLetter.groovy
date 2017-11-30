93e678901394e91a04abd20d654cfa8f202f8c28
coertvm <coertvm@outlook.com>
2017-11-30 09:09:15 +0000

    feat: Acknowledge Termination By Employee

---
+++ resources/System/scripts/AssignATEAcknowledgementLetter.groovy
@@ -0,0 +1,21 @@
+import com.egis.data.party.User
+import com.egis.DocumentModel
+import com.egis.party.GroupService
+import com.egis.kernel.db.DbManager
+import com.egis.kernel.Kernel
+
+DocumentModel doc = doc
+DbManager db = Kernel.get(DbManager.class)
+GroupService gs = Kernel.get(GroupService.class)
+
+String group = 'HCM'
+if (doc.role_level == 'C') {
+    group = 'COAM'
+} else if (doc.role_level == 'D to F') {
+    group = 'GM HR'
+}
+
+String name = gs.getMembers(group, null)[0]
+doc.signature().assignRole('_manager_', name)
+doc.workflow().set('_manager_', name)
+doc.parties.add(db.resolve(User.class, name))
---
+++ upgrade/ate_acknowledgement_letter.pdf.zip/custom.js
@@ -0,0 +1,13 @@
+
+//sap_no
+custom["c45f0fa5-72e6-6d57-87bd-5db3e68b9449"]={
+    onChange: function() {
+        form.form.sap_no.value = this.data.sap_no;
+        form.form.name.value = this.data.name;
+        form.form.email.value = this.data.email;
+        form.form.designation.value = this.data.designation;
+        form.form.division.value = this.data.division;
+    }
+}
+
+//--end
