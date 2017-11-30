9e1f07590f95b7eec468a76c58226bb011597fe6
coertvm <coertvm@outlook.com>
2017-11-30 11:53:51 +0000

    feat: Acknowledge Termination By Employee

--- upgrade/ate_acknowledgement_letter.pdf.zip/custom.js
+++ upgrade/ate_acknowledgement_letter.pdf.zip/custom.js
@@ -3,11 +3,10 @@
 custom["c45f0fa5-72e6-6d57-87bd-5db3e68b9449"]={
     onChange: function() {
         form.form.sap_no.value = this.data.sap_no;
-        form.form.name.value = this.data.name;
+        form.form.fullname.value = this.data.fullname;
         form.form.email.value = this.data.email;
         form.form.designation.value = this.data.designation;
         form.form.division.value = this.data.division;
     }
 }
-
 //--end
