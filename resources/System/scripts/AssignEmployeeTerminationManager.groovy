d2a6f1ffc7aac52e45fd0c1eaea639e5e25ec4df
coertvm <coertvm@outlook.com>
2018-01-11 13:38:31 +0000

    feat: Acknowledge Termination Employee

--- resources/System/scripts/AssignEmployeeTerminationManager.groovy
+++ resources/System/scripts/AssignEmployeeTerminationManager.groovy
@@ -1,6 +1,4 @@
 import com.egis.DocumentModel
-import com.egis.kernel.Kernel
-import com.egis.kernel.web.WebServer
 import com.egis.transnet.sap.Employee
 import com.egis.utils.ValidationUtils
 
@@ -8,8 +6,4 @@ DocumentModel doc = doc
 Employee manager = Employee.get(doc.sap_no).findManager()
 ValidationUtils.notNull(manager, "Cannot find manager for employee with SAP no $doc?.sap_no")
 manager.assignToRole(doc, "_manager_")
-
-String url = Kernel.get(WebServer.class).publicUrl
-url = url + "/public/file/${doc.docId.toString()}/Employee Termination Resignation Letter.pdf"
-doc.perm_link = url
 doc.save()
